18acd09e2447437f652e98f1c00f021c7ba485ec
Benji Heil <ben.heil.dev@gmail.com>
2026-05-27 20:10:59 -0400

    get first version of display working

---
+++ README.md
@@ -0,0 +1,10 @@
+## Weather Display
+
+A current weather monitor for Arduino R4 WiFi
+
+## Sample Weather Request
+
+```curl
+curl -X 'GET' 'https://api.weather.gov/stations/E0797/observations/latest' \
+          -H 'accept: application/geo+json'
+```
--- WeatherDisplay.ino
+++ WeatherDisplay.ino
@@ -8,6 +8,9 @@
  */
 
 #include "WiFiS3.h"
+#include <ArduinoJson.h>
+#include "ArduinoGraphics.h"
+#include "Arduino_LED_Matrix.h"
 
 #include "arduino_secrets.h"
 #include "lib.h"
@@ -17,9 +20,35 @@ char* PASS = SECRET_PASS;
 
 int status = WL_IDLE_STATUS;
 
+const char* SAMPLE_JSON = 
+"{ \
+    \"properties\": { \
+        \"temperature\": { \
+            \"value\": 1.0, \
+        } \
+    } \
+}";
+
 WiFiClient client;
+JsonDocument doc;
+ArduinoLEDMatrix matrix;
+
 
 void setup() {
+  deserializeJson(doc, SAMPLE_JSON);
+  Serial.begin(115200);
+  matrix.begin();
+
+  matrix.beginDraw();
+  matrix.stroke(0xFFFFFFFF);
+  float temp = doc["properties"]["temperature"]["value"];
+  String text = String((1.8*temp)+32); //convert C to F
+  matrix.textFont(Font_5x7);
+  matrix.beginText(0, 1, 0xFFFFFF);
+  matrix.println(text);
+  matrix.endText();
+
+  matrix.endDraw();
 }
 
 void loop() {
